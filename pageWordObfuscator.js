bd64046cccf8943c13ba6c207e533d8c2496c0d8
Silent Fraud <XSilent@Silents-MacBook-Pro.local>
2015-03-07 21:08:59 +0100

    Initial commit

---
+++ pageWordObfuscator.js
@@ -0,0 +1,13 @@
+/**
+ * Page Word Obfuscator
+ *
+ * @author 2015 Michael Neuhaus (XSilent)
+ */
+var pageWordObfuscator = function()
+{
+	this.obfuscateWord = function(word)
+	{
+		return word;
+	};
+
+};
